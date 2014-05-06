7c06abde3ceeda8601011b19e2006430dc993363
Pete Bachant <petebachant@gmail.com>
2014-05-05 23:28:45 -0400

    Case files as used for 2014 METS paper

--- README.md
+++ README.md
@@ -1,24 +1,6 @@
-unh-rvat-2d-rans
-================
-OpenFOAM (2.2.2) case files for a 2D RANS simulation of the UNH-RVAT cross-flow turbine in a towing tank. The simulation uses the `pimpleDyMFoam` solver and the `kOmegaSST` turbulence model. 
+UNH RVAT 2D OpenFOAM Case
+=========================
 
-Note that this simulation has not been verified or validated.
+Runs 4 processors
+Don't use wall functions for k at blades and shaft
 
-Python scripts
---------------
-There are some Python scripts included to perform various tasks:
-
-  * `calcperf.py` -- calculates performance of turbine, plots torque.
-  * `gendynmeshdict.py` -- generates a dynamicMeshDict to rotate the turbine. Note that by defaults the turbine
-                     rotates at an angular velocity that is slightly unsteady to match experiments.
-  * `send_email.py` -- can be customized to send an email when the simulation finishes.
-  * `qtprog.py` -- creates a progress bar using PyQt.
-
-Video
------
-A video of the vorticity contours produced by this simulation can be found here:
-http://youtu.be/AQ4EztjPEFk
-
-Acknowledgements
-----------------
-Thanks to Boloar from the cfd-online forums for providing a nice example case on which to base this one. Also thanks to vkrastev for additional advice.
--- gendynmeshdict.py
+++ gendynmeshdict.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 This script creates the dynamic mesh dictionary with periodic omega
@@ -9,89 +9,10 @@ the first peak at 80 degrees, to match experiments
 @author: pete
 """
 
-import matplotlib.pyplot as plt
-import numpy as np
-import re
+import foampy
 
-
-# Read endTime for simulation
-def getEndTime():
-    """Get run parameters"""
-    f = open("system/controlDict", "r")
-    for line in f.readlines():
-        if "endTime" in line:
-            endTime = re.findall("\d.\d+", line)
-            if endTime == []:
-                endTime = re.findall("\d+", line)
-    f.close()
-    endTime = np.float(endTime[0])
-    return endTime
-
-# Input parameters -- should probably make these args
 U = 1.0
 R = 0.5
 meantsr = 1.9
-meanomega = meantsr*U/R
-amp_omega = 3.7*2*np.pi/60.0 # amplitude of fluctuation is 3.7 RPM
-endTime = getEndTime()
-t = np.linspace(0, endTime, 400)
-omega = meanomega + amp_omega*np.sin(3*meanomega*t - np.pi/1.2)
 
-# Write to file
-top = """\
-/*--------------------------------*- C++ -*----------------------------------*\
-| =========                 |                                                 |
-| \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox           |
-|  \\    /   O peration     | Version:  2.2.2                                 |
-|   \\  /    A nd           | Web:      www.OpenFOAM.org                      |
-|    \\/     M anipulation  |                                                 |
-\*---------------------------------------------------------------------------*/
-FoamFile
-{
-    version     2.0;
-    format      ascii;
-    class       dictionary;
-    object      dynamicMeshDict;
-}
-
-
-dynamicFvMesh   solidBodyMotionFvMesh;
-
-motionSolverLibs ( "libfvMotionSolvers.so" );
-
-solidBodyMotionFvMeshCoeffs
-{
-    cellZone        AMIsurface_z;
-
-    solidBodyMotionFunction  rotatingMotion;
-    rotatingMotionCoeffs
-    {
-	origin		(0 0 0);
-	axis		(0 0 1);
-        omega		table
-	(
-"""
-
-bottom = """
-        );
-    }
-}"""
-
-"""Table should be in form
-		(t0 omega0)
-		(t1 omega1)
-"""
-
-table = ""
-
-for n in xrange(len(t)-1):
-    table += "            (" + str(t[n]) + "  " + str(omega[n]) + ")\n"
-table += "            (" + str(t[-1]) + "  " + str(omega[-1]) + ")"
-
-alltxt = top + table + bottom
-
-
-with open("constant/dynamicMeshDict", "w") as f:
-    f.write(alltxt)
-
-
+foampy.gen_dynmeshdict(U, R, meantsr, cellzone="AMIsurface_z", npoints=500)
