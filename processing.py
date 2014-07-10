c8855fe0d53f9702e26de530bb02335f339ef6f6
Pete Bachant <petebachant@gmail.com>
2014-07-10 01:42:20 -0400

    Add globals to processing module

--- processing.py
+++ processing.py
@@ -13,6 +13,7 @@ import sys
 area = 0.05
 R = 0.5
 U_infty = 1.0
+rho = 1000.0
 
 calc_perf(plot=False):
     t, torque, drag = foampy.load_all_torque_drag()
@@ -31,11 +32,11 @@ calc_perf(plot=False):
     # Compute power coefficient
     area = 0.05
     power = torque*omega
-    cp = power/(0.5*1000*area*1**3)
+    cp = power/(0.5*rho*area*U_infty**3)
     meancp = np.mean(cp[i:i2])
     print("Mean cp:", meancp)
     # Compute drag coefficient
-    cd = drag/(0.5*1000*area*1**2)
+    cd = drag/(0.5*rho*area*U_infty**2)
     meancd = np.mean(cd[i:i2])
     print("Mean cd:", meancd)
     if plot:
