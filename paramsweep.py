d49ecc8c66c58ce244cb6107e7776ab1c8f9e76e
Pete Bachant <petebachant@gmail.com>
2015-07-26 09:10:53 -0400

    Update paramsweep.py from verificationSA branch

--- paramsweep.py
+++ paramsweep.py
@@ -45,13 +45,12 @@ def set_maxco(maxco):
     maxco = str(maxco)
     foampy.dictionaries.replace_value("system/controlDict", "maxCo", maxco)
 
-def spatial_grid_dep(newfile=True):
+def spatial_grid_dep(newfile=True, nx_list=range(50, 95, 5)):
     if newfile:
         try:
             os.remove("processed/spatial_grid_dep.csv")
         except OSError:
             pass
-    nx_list = [42, 48, 58, 68, 78, 88, 98]
     for nx in nx_list:
         call("./Allclean")
         print("Setting blockMesh nX to {}".format(nx))
@@ -65,13 +64,13 @@ def timestep_dep(newfile=True):
             os.remove("processed/timestep_dep.csv")
         except OSError:
             pass
-    dt_list = [4e-3, 3e-3, 2.5e-3, 2e-3, 1.8e-3, 1.5e-3, 1e-3, 9e-4, 8e-4]
-    call("./Allrun.pre")
+    dt_list = [0.004, 0.003, 0.0025, 0.002, 0.0015, 0.00125, 0.001, 9e-4, 8e-4, 7e-4]
+    call("scripts/Allrun.pre")
     for dt in dt_list:
-        call("./Allclean.nomesh")
+        call("scripts/Allclean.nomesh")
         print("Setting timestep to {}".format(dt))
         set_timestep(dt)
-        call("./Allrun.postmesh")
+        call("scripts/Allrun.postmesh")
         processing.log_perf("timestep_dep.csv", verbose=False)
         
 def maxco_dep(newfile=True):
@@ -81,12 +80,12 @@ def maxco_dep(newfile=True):
         except OSError:
             pass
     maxco_list = [40, 20, 10, 5, 2, 0.9, 0.5]
-    call("./Allrun.pre")
+    call("scripts/Allrun.pre")
     for maxco in maxco_list:
-        call("./Allclean.nomesh")
+        call("scripts/Allclean.nomesh")
         print("Setting maxCo to {}".format(maxco))
         set_maxco(maxco)
-        call("./Allrun.postmesh")
+        call("scripts/Allrun.postmesh")
         processing.log_perf("maxco_dep.csv", verbose=False)
 
 def tsr_dep(newfile=True):
@@ -97,15 +96,16 @@ def tsr_dep(newfile=True):
             os.remove("processed/tsr_dep.csv")
         except OSError:
             pass
-    tsr_list = [1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5]
-    call("./Allrun.pre")
+    tsr_list = [3.25, 2.75, 2.25, 1.75, 1.25, 0.75, 0.5]
+    call("scripts/Allrun.pre")
     for tsr in tsr_list:
-        call("./Allclean.nomesh")
+        call("scripts/Allclean.nomesh")
         print("Setting tip speed ratio to {}".format(tsr))
-        call("./Allrun.postmesh {}".format(tsr), shell=True)
+        call("scripts/Allrun.postmesh {}".format(tsr), shell=True)
         processing.log_perf("tsr_dep.csv", verbose=False)
                             
 if __name__ == "__main__":
+    spatial_grid_dep(newfile=True)
 #    timestep_dep(newfile=True)
-    tsr_dep(newfile=False)
-
+#    tsr_dep(newfile=False)
+    
